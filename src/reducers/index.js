64b76262baf5aad046f60c499563923ac79b9deb
oh000024 <jakeohcs@gmail.com>
2017-12-14 00:02:09 -0500

    [Del] reducer folder

--- src/reducers/index.js
+++
@@ -1,8 +0,0 @@
-// import {combineReducers} from 'redux';
-// import TodoReducer from './reducer_todolist';
-//
-// const rootReducer = combineReducers({
-//   todolist = TodoReducer
-// })
-//
-// export default rootReducer;
